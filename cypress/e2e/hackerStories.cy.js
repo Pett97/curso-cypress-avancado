a262654a7caf54308b6e2aa72ddd28547147244a
Pett97 <henriquepeterson138@gmail.com>
2024-07-24 10:14:30 -0300

    ajustado contexto para exercico4 extras

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -4,7 +4,7 @@ describe("Hacker Stories", () => {
   const initialTerm = "React";
   const newTerm = "Cypress";
 
-  context.only("Batendo API real", () => {
+  context("Batendo API real", () => {
     beforeEach(() => {
       cy.intercept("GET", `**/search?query=${initialTerm}&page=0`).as(
         "getStories"
@@ -47,50 +47,31 @@ describe("Hacker Stories", () => {
       cy.get(`button:contains(${newTerm})`).should("be.visible");
     });
 
+    
+
     //FIM CONTEXTO batendo api REAL
   });
 
-  it("shows the footer", () => {
-    cy.get("footer")
-      .should("be.visible")
-      .and("contain", "Icons made by Freepik from www.flaticon.com");
-  });
-
   context("List of stories", () => {
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I assert on the data?
-    // This is why this test is being skipped.
-    // TODO: Find a way to test it out.
-    it.skip("shows the right data for all rendered stories", () => {});
 
-    it('shows 20 stories, then the next 20 after clicking "More"', () => {
-      cy.intercept("GET", "**/search?query=React&page=0").as("getStories");
+    it("shows the footer", () => {
       cy.visit("/");
-
-      //cy.assertLoadingIsShownAndHidden()
-      //cy.contains('More').should('be.visible')
-
-      cy.get(".item").should("have.length", 20);
-
-      cy.contains("More").click();
-
-      cy.assertLoadingIsShownAndHidden();
-      cy.wait("@getStories");
-      cy.get(".item").should("have.length", 40);
+      cy.get("footer")
+        .should("be.visible")
+        .and("contain", "Icons made by Freepik from www.flaticon.com");
     });
 
-    it("shows only nineteen stories after dimissing the first story", () => {
+    it.skip("shows the right data for all rendered stories", () => {});
+
+    
+
+    it.skip("shows only nineteen stories after dimissing the first story", () => {
       cy.get(".button-small").first().click();
 
       cy.get(".item").should("have.length", 19);
     });
 
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I test ordering?
-    // This is why these tests are being skipped.
-    // TODO: Find a way to test them out.
+  
     context.skip("Order by", () => {
       it("orders by title", () => {});
 
@@ -106,19 +87,8 @@ describe("Hacker Stories", () => {
     const initialTerm = "React";
     const newTerm = "Cypress";
 
-    beforeEach(() => {
-      cy.intercept({
-        method: "GET",
-        pathname: "**/search",
-        query: {
-          query: `${newTerm}`,
-          page: "0",
-        },
-      }).as("getNewTermsStories");
-      cy.get("#search").clear();
-    });
 
-    it("types and hits ENTER", () => {
+    it.skip("types and hits ENTER", () => {
       cy.get("#search").type(`${newTerm}{enter}`);
 
       cy.wait("@getNewTermsStories");
@@ -128,7 +98,7 @@ describe("Hacker Stories", () => {
       cy.get(`button:contains(${initialTerm})`).should("be.visible");
     });
 
-    it("types and clicks the submit button", () => {
+    it.skip("types and clicks the submit button", () => {
       cy.get("#search").type(newTerm);
       cy.contains("Submit").click();
 
@@ -139,18 +109,9 @@ describe("Hacker Stories", () => {
       cy.get(`button:contains(${initialTerm})`).should("be.visible");
     });
 
-    //it.only("types and submits the form directly", () => {
-    //  cy.get('form input[type="text"]')
-    //    .clear()
-    //    .type("cypress");
-    //  cy.get("form").submit();
-    //  //cy.wait("@getNewTermsStories");
-    //  //cy.get(".item").should("have.length", 20);
-    //  //cy.get('form input[type="text"]').should("have.value", "cypress");
-    //});
-    //
+
     context("Last searches", () => {
-      it("shows a max of 5 buttons for the last searched terms", () => {
+      it.skip("shows a max of 5 buttons for the last searched terms", () => {
         const faker = require("faker");
 
         cy.intercept({
